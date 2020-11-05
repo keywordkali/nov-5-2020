6727bf04637b2a283e5cc9df1d8e6fb59370c648
Kali Correll <kalicorrell18@gmail.com>
2020-11-05 11:10:14 -0500

    Updated Functions

--- js/student.js
+++ js/student.js
@@ -14,7 +14,24 @@ console.log(this.name, this.favcolor,this.favnumber);
 }
 }
 
-let fred = new Student("Fred", "green", 8);
-let wilma = new Student("Wilma","red", 1);
-fred.log();
-wilma.log();
+let students = [
+  new Student("Fred", "green", 8),
+  new Student("Wilma","red", 1),
+  new Student("Barney", "blue", 7),
+  new Student("Betty","orange", 3),
+  new Student("Pebbles","pink", 2)
+];
+
+const display = () => {
+    let tbody = document.getElementById("tbody");
+    tbody.innerHTML = "";
+    for(let s of students){
+     let tr = "<tr>";
+     tr += `<td>${s.name}</td>`;
+     tr += `<td>${s.favcolor}</td>`;
+     tr += `<td>${s.favnumber}</td>`;
+     tr += "</tr>";
+     tbody.innerHTML += tr;
+       
+    }
+}
